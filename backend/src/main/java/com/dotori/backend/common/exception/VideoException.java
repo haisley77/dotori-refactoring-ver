2e31cb7a44b151aacf5c57e5a3e1e42ad576eb39
haisley77 <hahi1108@gmail.com>
2025-01-01 18:58:30 +0900

    mod: VideoException 오버라이드 메서드 삭제

--- backend/src/main/java/com/dotori/backend/common/exception/VideoException.java
+++ backend/src/main/java/com/dotori/backend/common/exception/VideoException.java
@@ -13,13 +13,4 @@ public class VideoException extends BusinessException {
         this.errorCode = errorCode;
     }
 
-    // 예외가 발생했을 때 StackTrace를 채우는 역할을 하는 메서드
-    // StackTrace -> 예외 생성 비용 증가
-    // (Stack Depth 10 -> 4000ns 소요 -> 1~5ms
-    // 오버라이딩 -> 80ns 정도로 성능 향상
-    @Override
-    public synchronized Throwable fillInStackTrace() {
-        // StackTrace를 채우지 않도록 처리
-        return this;
-    }
 }
