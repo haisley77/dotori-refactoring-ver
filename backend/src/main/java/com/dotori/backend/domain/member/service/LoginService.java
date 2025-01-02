c2fb9718966b6b462cc00cd5ef89345038ff7079
haisley77 <hahi1108@gmail.com>
2025-01-02 18:38:32 +0900

    feat: import

--- backend/src/main/java/com/dotori/backend/domain/member/service/LoginService.java
+++ backend/src/main/java/com/dotori/backend/domain/member/service/LoginService.java
@@ -1,5 +1,6 @@
 package com.dotori.backend.domain.member.service;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,9 +20,9 @@ public class LoginService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Member member = memberRepository.findByEmail(email)
-			.orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 존재하지 않습니다."));
+			.orElseThrow(() -> new UsernameNotFoundException(email));
 
-		return org.springframework.security.core.userdetails.User.builder()
+		return User.builder()
 			.username(member.getEmail())
 			.roles(member.getRole().name())
 			.build();
