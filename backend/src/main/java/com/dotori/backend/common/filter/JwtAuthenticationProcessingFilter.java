901bddbb9bbbdc859d892479a6a0bdc53e5f1d16
haisley77 <hahi1108@gmail.com>
2025-01-06 23:03:01 +0900

    refactor: doFilterInternal 로직

--- backend/src/main/java/com/dotori/backend/common/filter/JwtAuthenticationProcessingFilter.java
+++ backend/src/main/java/com/dotori/backend/common/filter/JwtAuthenticationProcessingFilter.java
@@ -10,6 +10,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dotori.backend.common.exception.BusinessException;
+import com.dotori.backend.common.exception.ErrorCode;
+import com.dotori.backend.domain.member.service.RedisService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -20,7 +23,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.dotori.backend.domain.member.model.entity.Member;
 import com.dotori.backend.domain.member.repository.MemberRepository;
 import com.dotori.backend.domain.member.service.JwtService;
-import com.dotori.backend.domain.member.service.RedisService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,13 +30,13 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * Jwt 인증 필터
  * "/login" 이외의 URI 요청이 왔을 때 처리하는 필터
- * <p>
+ *
  * 기본적으로 사용자는 요청 헤더에 AccessToken만 담아서 요청
- * AccessToken 만료 시에만 RefreshToken을 요청 헤더에 AccessToken과 함께 요청
- * <p>
- * 1. RefreshToken이 없고, AccessToken이 유효한 경우 -> 인증 성공 처리, RefreshToken을 재발급하지는 않는다.
- * 2. RefreshToken이 없고, AccessToken이 없거나 유효하지 않은 경우 -> 인증 실패 처리, 403 ERROR
- * 3. RefreshToken이 있는 경우 -> DB의 RefreshToken과 비교하여 일치하면 AccessToken 재발급, RefreshToken 재발급(RTR 방식)
+ * AccessToken 만료 시에만 refreshToken을 요청 (헤더에 AccessToken과 함께 요청)
+ *
+ * 1. refreshToken이 없고, AccessToken이 유효한 경우 -> 인증 성공 처리, refreshToken을 재발급하지는 않는다.
+ * 2. refreshToken이 없고, AccessToken이 없거나 유효하지 않은 경우 -> 인증 실패 처리, 403 ERROR
+ * 3. refreshToken이 있는 경우 -> DB의 refreshToken과 비교하여 일치하면 AccessToken 재발급, refreshToken 재발급(RTR 방식)
  * 인증 성공 처리는 하지 않고 실패 처리
  */
 @Slf4j
@@ -48,38 +50,33 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
-		log.info("경로확인로그: {}", request.getRequestURI());
+
 		if (request.getRequestURI().contains(NO_CHECK_URL)) {
 			filterChain.doFilter(request, response);
-			return; // return으로 이후 현재 필터 진행 막기 (안해주면 아래로 내려가서 계속 필터 진행시킴)
+			return;		// 필터 진행 막기
 		}
 
-		// accesstoken 만료여부체크
-		String accessToken = jwtService.extractAccessToken(request).orElse(null);
-		if (accessToken != null) { //accesstoken이 있으면
-			if (!jwtService.isTokenValid(accessToken)) {
-				// 액세스 토큰이 유효하지 않을 때, 401 상태 코드와 함께 에러 메시지를 클라이언트에 반환
-				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				response.getWriter().write("{\"error\": \"Access Token is invalid\"}");
-				response.setContentType("application/json");
-				return;
+		Optional<String> accessTokenOpt = jwtService.extractAccessToken(request);
 
-			} else {
-				Optional<String> email = jwtService.extractEmailFromAccessToken(request);
-				Optional<String> role = jwtService.extractroleFromAccessToken(request);
-				log.info("email:{}", email);
-				log.info("role:{}", role);
-				if (email.isPresent() && role.isPresent()) {
-					Member member = memberRepository.findByEmail(email.get()).orElse(null);
-					SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.get());
-					List<GrantedAuthority> authorities = Collections.singletonList(authority);
+		if (accessTokenOpt.isPresent()) {
+			String accessToken = accessTokenOpt.get();
 
-					// Authentication 객체 생성
-					Authentication authentication = new UsernamePasswordAuthenticationToken(member, null, authorities);
-					SecurityContextHolder.getContext().setAuthentication(authentication);
-				}
+			String email = jwtService.extractEmailFromAccessToken(accessToken)
+					.orElseThrow(() -> new BusinessException(ErrorCode.EMAIL_NOT_FOUND));
 
-			}
+			String role = jwtService.extractRoleFromAccessToken(accessToken)
+					.orElseThrow(() -> new BusinessException(ErrorCode.ROLE_NOT_FOUND));
+
+			Member member = memberRepository.findByEmail(email)
+					.orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+
+			SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role);
+			List<GrantedAuthority> authorities = Collections.singletonList(authority);
+
+			// Authentication 객체 생성
+			Authentication authentication = new UsernamePasswordAuthenticationToken(member, null, authorities);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+
 			filterChain.doFilter(request, response);
 
 		} else {
