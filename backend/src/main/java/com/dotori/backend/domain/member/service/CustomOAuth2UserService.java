df6c36120f3bc0b30f9ba094e8213ee36b28c785
haisley77 <hahi1108@gmail.com>
2025-01-06 23:07:09 +0900

    refactor: 변수명 변경

--- backend/src/main/java/com/dotori/backend/domain/member/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/dotori/backend/domain/member/service/CustomOAuth2UserService.java
@@ -35,19 +35,10 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		log.info("CustomOAuth2UserService.loadUser() 실행 - OAuth2 로그인 요청 진입");
 
-		/**
-		 * DefaultOAuth2UserService 객체를 생성하여, loadUser(userRequest)를 통해 DefaultOAuth2User 객체를 생성 후 반환
-		 * DefaultOAuth2UserService의 loadUser()는 소셜 로그인 API의 사용자 정보 제공 URI로 요청을 보내서
-		 * 사용자 정보를 얻은 후, 이를 통해 DefaultOAuth2User 객체를 생성 후 반환한다.
-		 * 결과적으로, OAuth2User는 OAuth 서비스에서 가져온 유저 정보를 담고 있는 유저
-		 */
 		OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
 		OAuth2User oAuth2User = delegate.loadUser(userRequest);
-		/**
-		 * userRequest에서 registrationId 추출 후 registrationId으로 SocialType 저장
-		 * http://localhost:8080/oauth2/authorization/kakao에서 kakao가 registrationId
-		 * userNameAttributeName은 이후에 nameAttributeKey로 설정된다.
-		 */
+
+
 		String registrationId = userRequest.getClientRegistration().getRegistrationId();
 		SocialType socialType = getSocialType(registrationId);
 		String userNameAttributeName = userRequest.getClientRegistration()
@@ -82,11 +73,11 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 	}
 
 	private Member getUser(OAuth2Attributes attributes, SocialType socialType) {
-		Optional<Member> existingMember = memberRepository.findByEmail(attributes.getOauth2UserInfo().getEmail());
+		Optional<Member> member = memberRepository.findByEmail(attributes.getOauth2UserInfo().getEmail());
 
-		if (existingMember.isPresent()) {
+		if (member.isPresent()) {
 			// 기존 사용자가 존재하면 기존 사용자 정보를 그대로 반환
-			return existingMember.get();
+			return member.get();
 		} else {
 			// 새로운 사용자인 경우 사용자를 생성하고 저장
 			return saveUser(attributes, socialType);
@@ -94,7 +85,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 	}
 
 	/**
-	 * OAuthAttributes의 toEntity() 메소드를 통해 빌더로 User 객체 생성 후 반환
+	 * OAuthAttributes의 toEntity()메소드를 통해 빌더로 User 객체 생성 후 반환
 	 * 생성된 User 객체를 DB에 저장 : socialType, socialId, email, role 값만 있는 상태
 	 */
 	private Member saveUser(OAuth2Attributes attributes, SocialType socialType) {
